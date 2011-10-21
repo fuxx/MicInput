9cf42e5aee5d29cf0a816057857a82f18ebfbf1c
Stefan Popp <mail@stefanpopp.de>
2011-10-21 12:54:07 +0200

    Initial Commit

---
+++ MicInput/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MicInput
+//
+//  Created by Stefan Popp on 21.10.11.
+//  Copyright (c) 2011 SEMIKRON International GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MicInput/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MicInput
+//
+//  Created by Stefan Popp on 21.10.11.
+//  Copyright (c) 2011 SEMIKRON International GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
